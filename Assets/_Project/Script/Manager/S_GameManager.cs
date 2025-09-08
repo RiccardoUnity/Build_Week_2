c96bbcfcf3d0bd1307f18474b3ad259727109feb
RiccardoUnity <beninriccardo@gmail.com>
2025-09-08 20:04:34 +0200

    SetUp
    Commit iniziale
    Progetto vuoto

---
+++ Assets/_Project/Script/Manager/S_GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SGM
+{
+    public static class S_GameManager
+    {
+        public static string GetHorizontal() => "Horizontal";
+        public static string GetVertical() => "Vertical";
+    }
+}
