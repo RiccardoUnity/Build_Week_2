deda66498fae98169dd97018a08f1d52a959cf47
retroCyberdan <105318240+retroCyberdan@users.noreply.github.com>
2025-09-09 21:03:28 +0200

    PlayerController updates

--- Assets/_Project/Script/Player/PlayerController.cs
+++ Assets/_Project/Script/Player/PlayerController.cs
@@ -16,7 +16,16 @@ public class PlayerController : MonoBehaviour
     public float laneMultiplier = 80f;
     private int _laneInput = 1; // <- 0: sinistra   1: centro   2: destra
 
+    [Header("Jump Settings")]
     public float jumpForce = 7f;
+
+    [Header("Slide Settings")]
+    public Animator animator;
+    public float slideDuration = 1f;
+    private bool _isSliding = false;
+    private float _originalColliderHeight;
+    private Vector3 _originalColliderCenter;
+    private CapsuleCollider _capsule;
 
     [Header("Ground Check Settings")]
     public LayerMask groundLayerMask = 5;
@@ -30,7 +39,11 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        _rb.freezeRotation = true; // <- evita che il player cada di lato
+        _rb.freezeRotation = true;
+
+        _capsule = GetComponent<CapsuleCollider>();
+        _originalColliderHeight = _capsule.height;
+        _originalColliderCenter = _capsule.center;
     }
 
     private void FixedUpdate()
@@ -42,14 +55,14 @@ public class PlayerController : MonoBehaviour
     {
         ChangeLane();
 
-        CheckGround();
+        GroundChecker();
 
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
-        {
-            Jump();
-        }
+        if (Input.GetKeyDown(KeyCode.S)) StartCoroutine(Slide());
+
+        if (Input.GetKeyDown(KeyCode.W) && _isGrounded) Jump();
     }
 
+    #region Move and Lane changing
     private void Move() // <- gestisce il movimento continuo del Player
     {
         _horizontalInput = Input.GetAxis("Horizontal");
@@ -62,13 +75,13 @@ public class PlayerController : MonoBehaviour
 
     private void ChangeLane() // <- gestisce il cambio di corsia
     {
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             _laneInput--;
             if (_laneInput == -1) _laneInput = 0;
         }
 
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.D))
         {
             _laneInput++;
             if (_laneInput == 3) _laneInput = 2;
@@ -82,6 +95,28 @@ public class PlayerController : MonoBehaviour
 
         transform.position = Vector3.Lerp(transform.position, newPos, laneMultiplier * Time.deltaTime);
     }
+    #endregion
+
+    #region Slide and Jump
+    private IEnumerator Slide() // <- gestisce la scivolata
+    {
+        _isSliding = true;
+
+        animator.SetBool("isSliding", true);
+
+        _capsule.height = 1f;
+        _capsule.center = new Vector3(0f, -.5f, 0f);
+
+        yield return new WaitForSeconds(slideDuration);
+
+        _capsule.height = _originalColliderHeight;
+        _capsule.center = _originalColliderCenter;
+
+        animator.SetBool("isSliding", false);
+
+        _isSliding = false;
+    }
+
 
     private void Jump() // <- gestisce il salto
     {
@@ -91,8 +126,10 @@ public class PlayerController : MonoBehaviour
 
         _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
+    #endregion
 
-    private void CheckGround() // <- check sphere sotto i piedi per verificare il contatto col terreno
+    #region Ground Checker
+    private void GroundChecker() // <- check sphere sotto i piedi per verificare il contatto col terreno
     {
         _isGrounded = Physics.CheckSphere(transform.position + Vector3.down * groundCheckOffset, 0.2f, groundLayerMask);
     }
@@ -104,4 +141,5 @@ public class PlayerController : MonoBehaviour
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position + Vector3.down * groundCheckOffset, groundCheckRadius);
     }
+    #endregion
 }
