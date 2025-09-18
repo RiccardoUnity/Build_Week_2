f0d0ec22cdb0ae28a5041bf1fe61f6ebf39f7909
alessandrodeamicis <aledeamicis8@gmail.com>
2025-09-18 21:25:33 +0200

    Added power ups ui

--- Assets/_Project/Script/UI/CoinUI.cs
+++ Assets/_Project/Script/UI/CoinUI.cs
@@ -2,22 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using Save = SGM.S_SaveManager;
 
 public class CoinUI : MonoBehaviour
 {
     [SerializeField] private CoinManager _coinManager;
     [SerializeField] private TextMeshProUGUI _coinText;
 
-    void OnEnable()
+    void Start()
     {
+        _coinText.text = Save.GetCoin().ToString();
         if (_coinManager != null)
             _coinManager.onCoinPickUp.AddListener(UpdateUI);
-    }
-
-    void OnDisable()
-    {
-        if (_coinManager != null)
-            _coinManager.onCoinPickUp.RemoveListener(UpdateUI);
     }
 
     void UpdateUI(int newCoinValue)
